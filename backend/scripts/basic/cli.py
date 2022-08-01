37538b120337a7cc87150756db911d9f7cb7f697
yano-kentaro <ynkn0829@gmail.com>
2022-08-01 20:50:49 +0900

    2022-08-01

---
+++ backend/scripts/basic/cli.py
@@ -0,0 +1,9 @@
+import sys
+
+print('==================== 実行結果 ====================', end='\n\n')
+
+print(sys.argv)
+print(sys.argv[0])
+print(sys.argv[1])
+
+print('\n==================== 実行結果 ====================', end='\n\n')
